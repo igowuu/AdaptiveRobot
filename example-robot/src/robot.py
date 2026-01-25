c791c2ec5800fd05a89edaa0dc58fe4859dfc144
igowuu <137836892+igowuu@users.noreply.github.com>
2026-01-25 18:22:46 -0500

    Update copyright information and file header

--- example-robot/src/robot.py
+++ example-robot/src/robot.py
@@ -1,6 +1,7 @@
-# Perry - FRC 5113 Robot Code
+# Perry - 5113 FRC code
 # Copyright (c) 2026 Jacob Taylor (igowu) <https://github.com/igowuu>
-#
+# Code from: <https://github.com/igowuu/AdaptiveRobot.git>
+
 # Licensed under the MIT License.
 # See https://opensource.org/licenses/MIT for details.
 
@@ -70,3 +71,4 @@ class Perry(AdaptiveRobot):
 
         self.drivetrain.request_forward_percent(forward_speed_pct, DrivePriority.TELEOP, "teleop")
         self.drivetrain.request_rotation_percent(rotation_speed_pct, DrivePriority.TELEOP, "teleop")
+
